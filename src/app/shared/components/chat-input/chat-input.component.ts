a8aef20bc25e4479a9237de8a8da6825bbb1e1fb
Maxim Marchenko <maxim.marchenko.bp@gmail.com>
2024-02-14 10:53:20 +0200

    Added: chat-input component

---
+++ src/app/shared/components/chat-input/chat-input.component.ts
@@ -0,0 +1,26 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {NgStyle} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
+@Component({
+  selector: 'app-chat-input',
+  standalone: true,
+  imports: [
+    NgStyle,
+    FormsModule
+  ],
+  templateUrl: './chat-input.component.html',
+  styleUrl: './chat-input.component.scss'
+})
+export class ChatInputComponent {
+  @Input() placeholder = 'Write your message...';
+  @Input() bgColor = '#FFFFFF'
+  @Input() inputType: 'text' | 'email' = 'text'
+  @Output() sendText = new EventEmitter<string>();
+
+  text: string = '';
+
+  sendMessage(): void {
+    this.sendText.emit(this.text);
+  }
+}
