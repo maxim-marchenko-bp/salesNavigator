10f684726c4ab3a00d37331ed302693b3e61fbdd
Maxim Marchenko <maxim.marchenko.bp@gmail.com>
2024-02-14 11:00:57 +0200

    Added: display chat info from chatInfoService, basic methods for work with web socket service, button for show and hide chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {ChatComponent} from "./shared/components/chat/chat.component";
+import { ChatComponent } from "./shared/components/chat/chat.component";
+import { HttpClientModule } from "@angular/common/http";
+import { ChatInfoService } from "./core/services/chat-info.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ChatComponent],
+  imports: [ CommonModule, RouterOutlet, ChatComponent, HttpClientModule ],
+  providers: [ ChatInfoService ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/components/chat/chat.component.ts
+++ src/app/shared/components/chat/chat.component.ts
@@ -1,15 +1,64 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { WebSocketService } from '../../../core/services/web-socket.service';
+import { ChatInputComponent } from "../chat-input/chat-input.component";
+import { AsyncPipe, NgClass, NgIf } from "@angular/common";
+import { ChatInfoService } from "../../../core/services/chat-info.service";
+import { Observable } from "rxjs";
+import { ChatInfo } from "../../models/chat-info.model";
 
 @Component({
   selector: 'app-chat',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    ChatInputComponent,
+    NgClass,
+    AsyncPipe,
+    NgIf
   ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit, OnDestroy {
+  showChat = true;
+  chatInfo$: Observable<ChatInfo> | undefined;
 
+  constructor(
+    private chatInfoService: ChatInfoService,
+    private websocketService: WebSocketService
+  ) { }
+
+  ngOnInit(): void {
+    this.chatInfo$ = this.chatInfoService.getChatInfo();
+    // this.initSocketConnection();
+  }
+
+  ngOnDestroy(): void {
+    this.disconnectSocket();
+  }
+
+  sendText(text: string): void {
+    console.log('send text: ', text)
+  }
+
+  sendEmail(email: string): void {
+    console.log('send email: ', email)
+  }
+
+  private initSocketConnection(): void {
+    this.websocketService.connectToServer();
+  }
+
+  private sendMessage(): void {
+    this.websocketService.sendMessage('hello');
+  }
+
+  private receiveMessage(): void {
+    this.websocketService.receiveMessage().subscribe(receivedMessage => {
+      console.log(receivedMessage);
+    });
+  }
+
+  private disconnectSocket() {
+    this.websocketService.disconnectFromServer();
+  }
 }
