cbc3dde57dae32056d85f19324c3bca5557de547
Maxim Marchenko <maxim.marchenko.bp@gmail.com>
2024-02-14 18:46:44 +0200

    Added: ngx-socket-io config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { SocketIoConfig, SocketIoModule } from "ngx-socket-io";
+
+const config: SocketIoConfig = {
+  url: 'http://localhost:8080', options: {
+    withCredentials: true,
+  }
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(SocketIoModule.forRoot(config)),
+  ]
 };
